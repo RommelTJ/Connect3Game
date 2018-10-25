4117de20a76f83aa5d7b8eebef32f8d10374efb1
Rommel Rico <me@rommelrico.com>
2018-10-25 08:28:07 -0700

    Adding dropIn method and setting it on click when tapping the imageViews

--- app/src/main/java/com/rommelrico/connect3game/MainActivity.kt
+++ app/src/main/java/com/rommelrico/connect3game/MainActivity.kt
@@ -2,8 +2,20 @@ package com.rommelrico.connect3game
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    fun dropIn(view: View) {
+        val counter = view as ImageView
+        counter.translationY = -1500f
+        counter.setImageResource(R.drawable.yellow)
+        counter.animate().translationYBy(1500f).setDuration(300).rotation(3600f)
+
+        Log.i("INFO", "Image received: ${counter}")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
