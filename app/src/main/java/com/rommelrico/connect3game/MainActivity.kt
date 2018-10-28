98b8e86f5499dfd32198eef6b964409025a652c5
Rommel Rico <me@rommelrico.com>
2018-10-27 18:49:46 -0700

    Alternating the color chips

--- app/src/main/java/com/rommelrico/connect3game/MainActivity.kt
+++ app/src/main/java/com/rommelrico/connect3game/MainActivity.kt
@@ -2,16 +2,26 @@ package com.rommelrico.connect3game
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
+    // 0 = yellow, 1 = red.
+    var activePlayer = 0
+
     fun dropIn(view: View) {
         val counter = view as ImageView
         counter.translationY = -1500f
-        counter.setImageResource(R.drawable.yellow)
+
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow)
+            activePlayer = 1
+        } else {
+            counter.setImageResource(R.drawable.red)
+            activePlayer = 0
+        }
+
         counter.animate().translationYBy(1500f).setDuration(300).rotation(3600f)
     }
 
