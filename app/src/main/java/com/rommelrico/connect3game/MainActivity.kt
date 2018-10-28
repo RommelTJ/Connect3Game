5a2fec79853cda5ddaf373f55b568549f68024e9
Rommel Rico <me@rommelrico.com>
2018-10-27 19:00:36 -0700

    Winning positions

--- app/src/main/java/com/rommelrico/connect3game/MainActivity.kt
+++ app/src/main/java/com/rommelrico/connect3game/MainActivity.kt
@@ -12,6 +12,12 @@ class MainActivity : AppCompatActivity() {
 
     // Game state
     var gameState = arrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
+    // Winning positions
+    val winningPositions = arrayOf(
+            intArrayOf(0, 1, 2), intArrayOf(3, 4, 5), intArrayOf(6, 7, 8),
+            intArrayOf(0, 3, 6), intArrayOf(1, 4, 7), intArrayOf(2, 5, 8),
+            intArrayOf(0, 4, 8), intArrayOf(2, 4, 6)
+    )
 
     fun dropIn(view: View) {
         val counter = view as ImageView
